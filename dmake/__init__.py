de93f5813ca10319c93841135fa3e3dc428d44b0
Pierre-Julien Grizel <pjgrizel@numericube.com>
2020-05-11 15:34:30 +0200

    Importing inital project
    Blackified
    
    Added basic Makefile
    
    Added a beautiful makefile linter
    
    Basic changes
    
    Wow, we got an automatic importing of Docker configurations now (needs intensive testing but the thing is - it works out of the box)
    
    Using alternate yaml module, renamed 'setup' into 'config' to avoid confusion with default python installer script
    
    Pretty formatting
    
    Dmake config works way better now (bugfixes)
    
    Basic config test works now
    
    Wow, test pass perfectly now. Gut.
    
    Fixed basic --help test
    
    Fixed readme
    
    Initial work
    
    Minor update to doc
    
    Installing requirements by hand
    
    Make error message clearer
    
    Check container creation
    
    Make change valid with recent Docker version
    
    Fixing this test
    
    Fixed .md file format
    
    GitBook: [master] 2 pages modified

---
+++ dmake/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__init__.py
+
+Created by Pierre-Julien Grizel et al.
+Copyright (c) 2019 NumeriCube. All rights reserved.
+
+DMAKE distributed make module
+"""
+# Python3 rocks :)
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .cmd import main
+
+__author__ = ""
+__copyright__ = "Copyright 2016, NumeriCube"
+__credits__ = ["Pierre-Julien Grizel"]
+__license__ = "CLOSED SOURCE"
+__version__ = "TBD"
+__maintainer__ = "Pierre-Julien Grizel"
+__email__ = "pjgrizel@numericube.com"
+__status__ = "Production"
+
+
+__all__ = ["main"]
